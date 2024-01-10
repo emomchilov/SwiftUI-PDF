4c317a211c51afaaea63be953da5ba5793312a14
Eden Momchilov <88549142+emomchilov@users.noreply.github.com>
2024-01-09 18:35:57 -0600

    Initial Commit

---
+++ SwiftUIPDFs/SwiftUIPDFsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPDFsApp.swift
+//  SwiftUIPDFs
+//
+//  Created by Eden Momchilov on 1/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPDFsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
